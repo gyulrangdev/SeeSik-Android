0baaa44d428e740b4f3b3d20f422e4ae5a1b27b3
sohyeonAn <ansh4393@gmail.com>
2017-04-10 01:45:20 +0900

    layout

--- app/src/main/java/org/androidtown/myapplication/MainActivity.java
+++ app/src/main/java/org/androidtown/myapplication/MainActivity.java
@@ -1,7 +1,7 @@
 package org.androidtown.myapplication;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +9,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public static class Monthlry extends AppCompatActivity {
+
+        @Override
+        protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_monthly);
+        }
     }
 }
