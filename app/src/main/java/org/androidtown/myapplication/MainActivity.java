9fbeecbcf1d9b39010e80ce374e8f1ad2f1196ca
GHHM <zozon1330@naver.com>
2017-05-13 04:28:04 +0900

    add 새쌉이
    basic animation of main screen

--- app/src/main/java/org/androidtown/myapplication/MainActivity.java
+++ app/src/main/java/org/androidtown/myapplication/MainActivity.java
@@ -2,31 +2,53 @@ package org.androidtown.myapplication;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.ImageSwitcher;
+import android.widget.ImageView;
+import android.widget.ViewSwitcher;
+
 
 public class MainActivity extends AppCompatActivity {
     public static final int REQUEST_CODE_MENU = 101;
     Button evaBtn;
+    Handler mHandler = new Handler();
+    ImageThread thread;
+    ImageSwitcher switcher;
+    boolean running;
+
     DataBase db;
+
+    db = new DataBase(MainActivity.this);
+        db.createTable();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        evaBtn = (Button)findViewById(R.id.evaluationBtn);
-
-        evaBtn.setOnClickListener(new View.OnClickListener(){
+        evaBtn = (Button) findViewById(R.id.evaluationBtn);
+        switcher = (ImageSwitcher) findViewById(R.id.switcher);
+        evaBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(getApplicationContext(),DailyEvaluation.class);
+                Intent intent = new Intent(getApplicationContext(), DailyEvaluation.class);
                 startActivity(intent);
             }
         });
 
-        db = new DataBase(MainActivity.this);
-        db.createTable();
+        switcher.setFactory(new ViewSwitcher.ViewFactory() {
+            public View makeView() {
+                ImageView imageView = new ImageView(getApplicationContext());
+                imageView.setBackgroundColor(0xFF000000);
+                imageView.setScaleType(ImageView.ScaleType.CENTER_INSIDE);
+                imageView.setLayoutParams(new ImageSwitcher.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT));
+                return imageView;
+            }
+        });
+
 
     }
 
@@ -36,6 +58,51 @@ public class MainActivity extends AppCompatActivity {
         protected void onCreate(Bundle savedInstanceState) {
             super.onCreate(savedInstanceState);
             setContentView(R.layout.activity_monthly);
+        }
+    }
+
+//    private void startAnimation(){
+//
+//        switcher.setVisibility(View.VISIBLE);
+//        thread = new ImageThread();
+//        thread.start();
+//    }
+//    private void stopAnimation(){
+//        running = false;
+//        try{
+//            thread.join();
+//        }catch (InterruptedException e){}
+//        switcher.setVisibility(View.INVISIBLE);
+//    }
+
+    class ImageThread extends Thread{
+        int duration = 250;
+        final int imageId[] = {R.drawable.closeeyes, R.drawable.openeyes};
+        int currentIndex=0;
+
+        @Override
+        public void run() {
+            running = true;
+
+            while(running){
+                synchronized (this){
+                    mHandler.post(new Runnable() {
+                        @Override
+                        public void run() {
+                            switcher.setImageResource(imageId[currentIndex]);
+                        }
+                    });
+
+                    currentIndex++;
+                    if(currentIndex==imageId.length){
+                        currentIndex=0;
+                    }
+
+                    try{
+                        Thread.sleep(duration);
+                    }catch(InterruptedException e){}
+                }
+            }
         }
     }
 }
