66b5200a40da13b45a486c1307563c360a18a619
GHHM <zozon1330@naver.com>
2017-05-13 04:03:59 +0900

    Graph animation
    add 'grow.xml'

--- app/src/main/java/org/androidtown/myapplication/DailyEvaluation.java
+++ app/src/main/java/org/androidtown/myapplication/DailyEvaluation.java
@@ -1,36 +1,68 @@
 package org.androidtown.myapplication;
 
 import android.app.ProgressDialog;
+import android.content.res.Resources;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.LinearLayout;
 import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import static android.R.attr.name;
+import static org.androidtown.myapplication.R.id.naBar;
 
 public class DailyEvaluation extends AppCompatActivity {
+    LinearLayout mainLayout;
     ProgressDialog dialog;
+    Resources res;
+    Animation growAnim;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_daily_evaluation);
+        res=getResources();
+        growAnim = AnimationUtils.loadAnimation(this,R.anim.grow);
 
         ProgressBar naBar = (ProgressBar) findViewById(R.id.naBar);
         naBar.setIndeterminate(false);
         naBar.setMax(100);
         naBar.setProgress(80);
+        naBar.setAnimation(growAnim);
 
         ProgressBar cholBar = (ProgressBar) findViewById(R.id.cholBar);
         naBar.setIndeterminate(false);
         naBar.setMax(100);
         naBar.setProgress(80);
+        cholBar.setAnimation(growAnim);
 
         ProgressBar fatBar = (ProgressBar) findViewById(R.id.fatBar);
         naBar.setIndeterminate(false);
         naBar.setMax(100);
         naBar.setProgress(80);
+        fatBar.setAnimation(growAnim);
 
         ProgressBar sugarBar = (ProgressBar) findViewById(R.id.sugarBar);
         naBar.setIndeterminate(false);
         naBar.setMax(100);
         naBar.setProgress(80);
+        sugarBar.setAnimation(growAnim);
+    }
+
+
+    @Override
+    public void onWindowFocusChanged(boolean hasFocus) {
+        super.onWindowFocusChanged(hasFocus);
+        Toast.makeText(this,"그래프 애니메이션 활성화 : "+hasFocus,Toast.LENGTH_SHORT).show();
+
+        if(hasFocus){
+            growAnim.start();
+        }
+        else{
+            growAnim.reset();
+        }
     }
 }
--- app/src/main/java/org/androidtown/myapplication/MainActivity.java
+++ app/src/main/java/org/androidtown/myapplication/MainActivity.java
@@ -1,15 +1,30 @@
 package org.androidtown.myapplication;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
+    public static final int REQUEST_CODE_MENU = 101;
+    Button evaBtn;
     DataBase db;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        evaBtn = (Button)findViewById(R.id.evaluationBtn);
+
+        evaBtn.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(),DailyEvaluation.class);
+                startActivity(intent);
+            }
+        });
+
         db = new DataBase(MainActivity.this);
         db.createTable();
 
