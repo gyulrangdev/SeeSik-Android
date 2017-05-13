165a398d3c09c693e53955ec34409ebf2f931014
GHHM <zozon1330@naver.com>
2017-05-13 09:04:04 +0900

    Revert "opacity 00"
    This reverts commit ce6d7440cb4bcf25b8fab6e17e63427a1eda444c.

--- app/src/main/java/org/androidtown/myapplication/MainActivity.java
+++ app/src/main/java/org/androidtown/myapplication/MainActivity.java
@@ -9,10 +9,7 @@ import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.ImageSwitcher;
 import android.widget.ImageView;
-import android.widget.Toast;
 import android.widget.ViewSwitcher;
-
-import static android.R.attr.data;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -23,10 +20,10 @@ public class MainActivity extends AppCompatActivity {
     ImageSwitcher switcher;
     boolean running;
 
-//    DataBase db;
-//
-//    db = new DataBase(MainActivity.this);
-//        db.createTable();
+    DataBase db;
+
+    db = new DataBase(MainActivity.this);
+        db.createTable();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,22 +35,21 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(getApplicationContext(), DailyEvaluation.class);
-                startActivityForResult(intent, REQUEST_CODE_MENU);
+                startActivity(intent);
             }
         });
-
 
         switcher.setFactory(new ViewSwitcher.ViewFactory() {
             public View makeView() {
                 ImageView imageView = new ImageView(getApplicationContext());
-                imageView.setBackgroundColor(0x00000000);
+                imageView.setBackgroundColor(0xFF000000);
                 imageView.setScaleType(ImageView.ScaleType.CENTER_INSIDE);
                 imageView.setLayoutParams(new ImageSwitcher.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT));
                 return imageView;
             }
         });
 
-        startAnimation();
+
     }
 
     public static class Monthlry extends AppCompatActivity {
@@ -65,60 +61,46 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-        if (requestCode == REQUEST_CODE_MENU) {
-            Toast.makeText(getApplicationContext(), "요청 코드: " + requestCode + " 결과 코드: " + resultCode, Toast.LENGTH_LONG).show();
+//    private void startAnimation(){
+//
+//        switcher.setVisibility(View.VISIBLE);
+//        thread = new ImageThread();
+//        thread.start();
+//    }
+//    private void stopAnimation(){
+//        running = false;
+//        try{
+//            thread.join();
+//        }catch (InterruptedException e){}
+//        switcher.setVisibility(View.INVISIBLE);
+//    }
 
-            if (resultCode == RESULT_OK) {
-                String msg = data.getExtras().getString("msg");
-                Toast.makeText(getApplicationContext(), msg, Toast.LENGTH_LONG).show();
-            }
-        }
-    }
-
-    private void startAnimation() {
-
-        switcher.setVisibility(View.VISIBLE);
-        thread = new ImageThread();
-        thread.start();
-    }
-
-    private void stopAnimation() {
-        running = false;
-        try {
-            thread.join();
-        } catch (InterruptedException e) {
-        }
-        switcher.setVisibility(View.INVISIBLE);
-    }
-
-    class ImageThread extends Thread {
-        int duration = 1000;
+    class ImageThread extends Thread{
+        int duration = 250;
         final int imageId[] = {R.drawable.closeeyes, R.drawable.openeyes};
-        int currentIndex = 0;
+        int currentIndex=0;
 
         @Override
         public void run() {
             running = true;
 
-            while (running) {
-                synchronized (this) {
+            while(running){
+                synchronized (this){
                     mHandler.post(new Runnable() {
                         @Override
                         public void run() {
                             switcher.setImageResource(imageId[currentIndex]);
                         }
                     });
+
                     currentIndex++;
-                    if (currentIndex == imageId.length) {
-                        currentIndex = 0;
+                    if(currentIndex==imageId.length){
+                        currentIndex=0;
                     }
-                    try {
+
+                    try{
                         Thread.sleep(duration);
-                    } catch (InterruptedException e) {
-                    }
+                    }catch(InterruptedException e){}
                 }
             }
         }
