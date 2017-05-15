eb92bc0a473524b33a5075126023ce50d1a1463f
Jaena <jaena96@naver.com>
2017-05-16 00:03:27 +0900

    fix error in mainActivity

--- app/src/main/java/org/androidtown/myapplication/MainActivity.java
+++ app/src/main/java/org/androidtown/myapplication/MainActivity.java
@@ -35,7 +35,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         db = new DataBase(MainActivity.this);
-        db.createTable();
         evaBtn = (Button) findViewById(R.id.evaluationBtn);
         selectDietBtn = (Button) findViewById(R.id.selectDietBtn);
         switcher = (ImageSwitcher) findViewById(R.id.switcher);
