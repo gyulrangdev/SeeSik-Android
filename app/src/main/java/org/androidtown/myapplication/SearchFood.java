538b8fc5ae0a5b53b0e22ee9823e892b74ca5bf4
Jaena <jaena96@naver.com>
2017-05-17 10:46:59 +0900

    Update SearchFood.java

--- app/src/main/java/org/androidtown/myapplication/SearchFood.java
+++ app/src/main/java/org/androidtown/myapplication/SearchFood.java
@@ -1,29 +1,99 @@
-//package org.androidtown.myapplication;
-//
-//import android.content.Intent;
-//import android.os.Bundle;
-//import android.support.v7.app.AppCompatActivity;
-//import android.widget.ArrayAdapter;
-//import android.widget.AutoCompleteTextView;
-//import android.widget.EditText;
-//
-//public class SearchFood extends AppCompatActivity {
-//    EditText searchBar;
-//    @Override
-//    protected void onCreate(Bundle savedInstanceState) {
-//        super.onCreate(savedInstanceState);
-//        setContentView(R.layout.activity_search_food);
-//
-//        searchBar = (EditText) findViewById(R.id.search);
-//
-//        AutoCompleteTextView textView = (AutoCompleteTextView) findViewById(R.id.search);
-//
-//        Intent intent = getIntent();
-//        String temp = intent.getStringExtra("foodName");
-//        String foodName[] = temp.split(",");
-//
-//        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,android.R.layout.simple_dropdown_item_1line,foodName);
-//        textView.setAdapter(adapter);
-//
-//    }
-//}
+package org.androidtown.myapplication;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import java.util.List;
+
+public class SearchFood extends AppCompatActivity {
+    EditText searchBar;
+    DataBase db;
+
+    ListView foodType;// 대분류
+    ListView foodsearchedList; //소분류
+    ListView intakeList; // 섭취 리스트
+
+    AutoCompleteTextView textView;
+
+    String foodName[]={""};// AutoCompleteTextView 안에 들어갈 String list
+    String [] foodTypeList = {"패스트푸드", "국", "튀김", "디저트","면","고기(족발, 보쌈 등)"};
+    String []foodList= {};
+
+    boolean searchFoodFirst = true;// searchfood에 처음으로 들어갈 때 구분 변수
+
+    StringForSearchFood stringFor;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_search_food);
+        db = new DataBase(SearchFood.this);
+        stringFor = new StringForSearchFood();
+
+        AboutFoodName(SearchFood.this);
+        AboutFoodType();
+        AboutFoodList();
+        AboutDailyIntakeList();
+    }
+
+    public void AboutFoodName(Context context)
+    {
+        textView = (AutoCompleteTextView) findViewById(R.id.searchBar);
+
+        if(searchFoodFirst)
+        {
+        foodName=db.getAllFoodName();// 모든 string을 db에서 받아와 foodName에 넣음
+            searchFoodFirst = false;
+        }
+
+        ArrayAdapter<String> SearchFoodAdapter = new ArrayAdapter<String>(this,android.R.layout.simple_dropdown_item_1line,foodName);
+        textView.setAdapter(SearchFoodAdapter);
+        textView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
+                String str = textView.getText().toString();
+                String temp = db.insertItemInList("foodList",str);
+                Toast.makeText(getApplicationContext(),temp,Toast.LENGTH_LONG).show();
+            }
+        });
+
+
+        // 자동완성에서 선택되면
+        // intakeList에 자동추가,
+        // text는 비워짐
+    }
+
+    public void AboutFoodType()
+    {
+        foodType = (ListView)findViewById(R.id.foodType);
+        ArrayAdapter<String> foodTypeAdapter = new ArrayAdapter<String>(this,R.layout.support_simple_spinner_dropdown_item,foodTypeList);
+        foodType.setAdapter(foodTypeAdapter);
+    }
+
+    public void AboutFoodList()
+    {
+        // foodList도 새롭게 바뀜(정확히 말하면 foodList를 구성하는 어레이가 바뀜)
+        // foodList종류가 여러가지! 그거 엑셀에 나눠서 분류해서 따로 Listㅇㅇㅇㅇㅇ 해서 정의? 해두자
+        foodsearchedList = (ListView)findViewById(R.id.foodList);//  소분류...
+        ArrayAdapter<String> foodListAdapter = new ArrayAdapter<String>(this,R.layout.support_simple_spinner_dropdown_item,foodList);
+        foodsearchedList.setAdapter(foodListAdapter);
+        //이것도 선택되면 intakeList에 자동추가
+    }
+
+    public void AboutDailyIntakeList()
+    {//커스텀 리스트 뷰 사용!
+        intakeList = (ListView) findViewById(R.id.intakeList);//이건 커스텀으로? 넣어야 할 듯....
+    }
+
+}
